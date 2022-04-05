a91fd51424b82414643e815c926dba154b60c33a
Guilherme da Silva Machado <1904706@SALAS.AULAS>
2022-04-04 21:16:04 -0300

    commit aula

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from asyncio.base_futures import _format_callbacks
-import re
-
 from flask import render_template, Flask, request
 
 app = Flask(__name__)
@@ -11,20 +8,25 @@ def index():
 
 @app.route('/calculadora', methods=['POST', 'GET'])
 def calculadora():
-    vl1 = int(request.form['inpt1'])
-    vl2 = int(request.form['inpt2'])
-    opr = request.form['inpt3']
+    vl1 = int(request.form['ipt1'])
+    vl2 = int(request.form['ipt2'])
+    opr = request.form['ipt3']
     
     if opr == 'soma':
         total = vl1 + vl2
         return render_template('index.html', total=total)
     elif opr == 'multiplicação':
+        if vl2 < 0:
+            return render_template('home.html')
         total = vl1 * vl2
         return render_template('index.html', total=total)
     elif opr == 'subtração':
         total = vl1 - vl2
         return render_template('index.html', total=total)
     elif opr == 'divisão':
+        if vl2 == 0:
+            return render_template('home.html')
         total = vl1 // vl2
+        print('teste')
         return render_template('index.html', total=total)
         
